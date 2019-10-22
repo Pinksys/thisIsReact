984c90a40ce7ae9d268ef9d7a0eb1e0b9cdd9d8f
Ramin Zarfeshan <raminzarf@gmail.com>
2019-10-21 22:59:34 -0400

    Updates

---
+++ precache-manifest.03d6fe89511ab33104e7a082b4072abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ec19fb55741c74e906a56985c842ae",
+    "url": "/thisIsReact/index.html"
+  },
+  {
+    "revision": "62ea11e184d7ab4d225e",
+    "url": "/thisIsReact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f53db1adaece63e36c1f",
+    "url": "/thisIsReact/static/js/2.2d862643.chunk.js"
+  },
+  {
+    "revision": "62ea11e184d7ab4d225e",
+    "url": "/thisIsReact/static/js/main.3c821c55.chunk.js"
+  },
+  {
+    "revision": "cf32f2b2929c5a830dfb",
+    "url": "/thisIsReact/static/js/runtime-main.571ba807.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/thisIsReact/static/media/logo.25bf045c.svg"
+  }
+]);
